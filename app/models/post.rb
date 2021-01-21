1f865871153084a808fd9d7e94f1d4ab6fdb9e86
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-01-20 18:55:48 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+    belongs_to :Author
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(fiction non-fiction),
+    message: "%{value} is not a valid reponse" }
+
+    # def valid_post
+    #     if self.title.include? "Won't Believe" || "Secret" || "Top" || "Guess"
+    #     end
+    # end
 end
