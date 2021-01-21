7f6f23fc05b042546a381206cd872e171dfe8fbb
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-01-20 18:43:27 -0600

    commit tree bd3f4edb2fb141c4bc8aff41e86fb39195355d46

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+    has_many :posts
+
+    validates :name, presence: true
+    validates :phone_number, length: {is: 10}
+    validates :name, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+    belongs_to :Author
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(fiction non-fiction),
+    message: "%{value} is not a valid reponse" }
 end
